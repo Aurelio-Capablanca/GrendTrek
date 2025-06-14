2b595408b3a2643253a88149add4c9fa4e98a368
Aurelio <aurelio.iempereur@gmail.com>
2025-06-14 10:21:08 -0600

    start

---
+++ src/main/java/com/aib/grendtrek/GrendTrekApplication.java
@@ -0,0 +1,13 @@
+package com.aib.grendtrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrendTrekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrendTrekApplication.class, args);
+    }
+
+}
