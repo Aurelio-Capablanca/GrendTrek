d57b86d586afe37f86c41e90d1c85dea26c0b648
Aurelio <aurelio.iempereur@gmail.com>
2025-06-19 23:51:39 -0600

    starting API
    + leaving out the main to do the API

--- src/main/java/com/aib/grendtrek/Exposure/ConfigurationForConnections.java
+++ src/main/java/com/aib/grendtrek/Exposure/ConfigurationForConnections.java
@@ -1,12 +1,22 @@
 package com.aib.grendtrek.Exposure;
 
+import com.aib.grendtrek.dataConfigurations.common.ConnectionAttributes;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+
+import java.util.List;
+
 @RestController
+@RequestMapping("api/grendTrek/configuration")
 public class ConfigurationForConnections {
 
-    public Mono<String> setDatabaseConfigurations(){
+    @PostMapping(value = "/setConections", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> setDatabaseConfigurations(@RequestBody List<ConnectionAttributes> attributes){
         return null;
     }
 
