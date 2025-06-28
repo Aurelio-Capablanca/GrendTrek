4d71e1b950fc5622b175f773e0713bb3611d9148
Aurelio <aurelio.iempereur@gmail.com>
2025-06-27 22:38:21 -0600

    Initial DDL generation for Create tables
    first, we consider how to create the tables in order to keep it reusable at many cases, then the thing is to issue and generate the Insert sentences Automatically; as both of those are the intentions and goals of this project, so first the thing is to handle how to create properly the table, but for now, just doing it as a recreation of the SQL Server content, still is missing to convert types for PostgreSQL

--- src/main/java/com/aib/grendtrek/dataTransformations/bridge/GenerateDDLForPostgreSQL.java
+++ src/main/java/com/aib/grendtrek/dataTransformations/bridge/GenerateDDLForPostgreSQL.java
@@ -2,10 +2,42 @@ package com.aib.grendtrek.dataTransformations.bridge;
 
 import com.aib.grendtrek.dataConfigurations.MicrosoftSQLServer.model.SchemaDataMSSQL;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class GenerateDDLForPostgreSQL {
 
-    public String createDDLForPostgreSQL(SchemaDataMSSQL schemaTable){
-        return "";
+    public List<String> createDDLForPostgreSQL(List<SchemaDataMSSQL> schemaTable) {
+        final Map<String, List<SchemaDataMSSQL>> tablesPerSchema = schemaTable
+                .stream()
+                .collect(Collectors
+                        .toMap(SchemaDataMSSQL::getTableName,
+                                List::of
+                        )
+                );
+        final List<String> DDLToCreate = new ArrayList<>();
+        tablesPerSchema.forEach((key, value) -> {
+            final StringBuilder DDLForTables = new StringBuilder()
+                    .append("create table ").append(key)
+                    .append("( ");
+            value.forEach(fields -> {
+                // ColumnName DataType(length) CONSTRAINT ConstraintName ConstraintType Not null (IsNullable),
+                DDLForTables.append(fields.getColumnName()).append(" ")
+                        .append(fields.getDataType());
+                if (fields.getLenghtField() == null)
+                    DDLForTables.append("(").append(fields.getLenghtField()).append(") ");
+                else
+                    DDLForTables.append(" ");
+                //if ()
+            });
+            DDLToCreate.add(DDLForTables.toString());
+        });
+        DDLToCreate.forEach(System.out::println);
+        return Collections.emptyList();
     }
 
 
