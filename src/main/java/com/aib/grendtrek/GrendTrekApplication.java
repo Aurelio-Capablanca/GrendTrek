740ff2052920ebd41eeb7ba403f91948cef26e70
Aurelio <aurelio.iempereur@gmail.com>
2025-07-17 20:47:45 -0600

    Goodbye R2DBC
    this was quite funny, but not that much since there's no real hope for a transactional framework here, so we're saying goodbye instead of getting drowned here, so goodbye and farewell, we see ya at Rust!!!

--- src/main/java/com/aib/grendtrek/GrendTrekApplication.java
+++ src/main/java/com/aib/grendtrek/GrendTrekApplication.java
@@ -12,47 +12,8 @@ import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 public class GrendTrekApplication {
 
     public static void main(String[] args) {
+        //Farewell !!! 
         SpringApplication.run(GrendTrekApplication.class, args);
-//        final R2DBCConnectionFactory factory = new R2DBCConnectionFactory();
-//        //Origin
-//        factory.addConnections("SQLServerADworks", ConnectionAttributes.builder()
-//                .databaseToConnect("AdventureWorks2022")
-//                .host("localhost")
-//                .driver("mssql")
-//                .user("sa")
-//                .password("jklgHnbvc555SS")
-//                .port(1433)
-//                .build());
-//        //Destiny
-//        factory.addConnections("PostgreSQLDestiny",
-//                ConnectionAttributes.builder()
-//                        .databaseToConnect("transcontinentalshippings")
-//                        .host("localhost")
-//                        .driver("postgresql")
-//                        .user("superuserp")
-//                        .password("jkl555")
-//                        .port(5432)
-//                        .build());
-//        // calling them
-//        final ConnectionFactory origin = factory.getConnectionFactory("SQLServerADworks");
-//        final ConnectionFactory destiny = factory.getConnectionFactory("PostgreSQLDestiny");
-//        factory.init(origin);
-//        factory.init(destiny);
-//        //gettin full schemas
-//        final QuerySetsForMSSQL originQuerySet = new QuerySetsForMSSQL();
-//        final QuerySetsForPostgreSQL destinySet = new QuerySetsForPostgreSQL();
-//
-//        originQuerySet.seeAllTablesBySchema(origin)
-//                .doOnNext(System.out::println)
-//                .thenMany(destinySet.seeAllTablesFromSchema(destiny).doOnNext(System.out::println))
-//                .thenMany(originQuerySet.seeByTableAndSchema(origin, "Employee", "HumanResources").doOnNext(System.out::println))
-//                .subscribe(res -> {},
-//                        error -> System.out.println("Error : " + error.getMessage()));
-//        System.out.println("Get a Table !");
-//
-//        originQuerySet.seeByTableAndSchema(origin, "Employee", "HumanResources").doOnNext(System.out::println).subscribe(results -> {
-//        }, error -> System.out.println("Error : " + error.getMessage()));
-
     }
 
 }
